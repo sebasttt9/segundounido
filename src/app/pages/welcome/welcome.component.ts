8ec088bd2c96acd4610f168a034bd12720a2dc3d
ebgarcia <ebgarcia@sudamericano.edu.ec>
2024-11-21 10:26:41 -0500

    menu

--- src/app/pages/welcome/welcome.component.ts
+++ src/app/pages/welcome/welcome.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { PickerComponent } from '../picker/picker.component';
+import { MenuComponent } from "../menu/menu.component";
 
 @Component({
   selector: 'app-welcome',
   standalone: true,
-  imports: [PickerComponent],
+  imports: [PickerComponent, MenuComponent],
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.css']
 })
