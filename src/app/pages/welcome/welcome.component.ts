30638533f4b548c467befae87ce54f6211d618ab
BParreno <biparreno@sudamericano.edu.ec>
2024-11-21 11:14:27 -0500

    commit ARREGLADO

--- src/app/pages/welcome/welcome.component.ts
+++ src/app/pages/welcome/welcome.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { CarlosAComponent } from '../../carlos-a/carlos-a.component';
+import { IconofComponent } from '../../iconof/iconof.component';
+import { PickerComponent } from '../picker/picker.component';
 import { SwitchBPComponent } from '../../switch-bp/switch-bp.component';
+import { MenuComponent } from '../menu/menu.component';
 
 @Component({
   selector: 'app-welcome',
   standalone: true,
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.css'],
-  imports: [SwitchBPComponent],
+  imports: [MenuComponent, CarlosAComponent, IconofComponent, PickerComponent, SwitchBPComponent],
 })
 export class WelcomeComponent implements OnInit {
 
